f90157e3378b4a243e59edc3b0b7dce24b5ac9d7
Tyler Durr <tylerdurr+github@gmail.com>
2024-02-05 14:16:36 -0600

    Spaces to tabs

--- Extensions/IComparableExtensions.cs
+++ Extensions/IComparableExtensions.cs
@@ -1,19 +1,19 @@
 ﻿namespace Extensions
 {
-    public static class IComparableExtensions
-    {
-        public static bool InRange<T>(this T comparable, T minimum, T maximum, bool minInclusive = true, bool maxInclusive = true) where T : IComparable<T>
-        {
-            ArgumentOutOfRangeException.ThrowIfGreaterThan(minimum, maximum);
-            if (minimum.Equals(maximum) && minInclusive != maxInclusive) throw new ArgumentException("Half-open degenerate interval specified.");
+	public static class IComparableExtensions
+	{
+		public static bool InRange<T>(this T comparable, T minimum, T maximum, bool minInclusive = true, bool maxInclusive = true) where T : IComparable<T>
+		{
+			ArgumentOutOfRangeException.ThrowIfGreaterThan(minimum, maximum);
+			if (minimum.Equals(maximum) && minInclusive != maxInclusive) throw new ArgumentException("Half-open degenerate interval specified.");
 
-            var minComp = comparable.CompareTo(minimum);
-            if (minComp < 0 || (minComp == 0 && !minInclusive)) return false;
+			var minComp = comparable.CompareTo(minimum);
+			if (minComp < 0 || (minComp == 0 && !minInclusive)) return false;
 
-            var maxComp = comparable.CompareTo(maximum);
-            if (maxComp > 0 || (maxComp == 0 && !maxInclusive)) return false;
+			var maxComp = comparable.CompareTo(maximum);
+			if (maxComp > 0 || (maxComp == 0 && !maxInclusive)) return false;
 
-            return true;
-        }
-    }
+			return true;
+		}
+	}
 }
