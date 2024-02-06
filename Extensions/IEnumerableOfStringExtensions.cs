0c4a57ef0617e34be4eaa8ccef3ea125a281f679
Tyler Durr <tylerdurr+github@gmail.com>
2024-02-05 21:38:38 -0600

    `.Join` and `.TextualJoin` extensions

---
+++ Extensions/IEnumerableOfStringExtensions.cs
@@ -0,0 +1,31 @@
+﻿namespace Extensions
+{
+	public static class IEnumerableOfStringExtensions
+	{
+		public static string Join(this IEnumerable<string> source, string separator) => string.Join(separator, source);
+
+		public static string TextualJoin(this IEnumerable<string> items, string conjunction = "and", bool complexItems = false)
+		{
+			ArgumentNullException.ThrowIfNull(items);
+			items = items.Select((item) =>
+			{
+				ArgumentNullException.ThrowIfNull(item, nameof(items));
+				ArgumentException.ThrowIfNullOrWhiteSpace(item, nameof(items));
+				return item.Trim();
+			});
+
+			ArgumentNullException.ThrowIfNull(conjunction);
+			ArgumentException.ThrowIfNullOrWhiteSpace(conjunction);
+			conjunction = conjunction.Trim();
+
+			var joiner = complexItems ? "; " : ", ";
+			switch (items.Count())
+			{
+				case 0: return string.Empty;
+				case 1: return items.Single();
+				case 2: return items.First() + " " + conjunction + " " + items.Last();
+				default: return string.Join(joiner, items.SkipLast(1)) + joiner + conjunction + " " + items.Last();
+			}
+		}
+	}
+}
---
+++ Tests/IEnumerableOfStringExtensions.cs
@@ -0,0 +1,29 @@
+﻿using Extensions;
+
+namespace Tests
+{
+	public class IEnumerableOfStringExtensions
+	{
+		[Theory]
+		[InlineData(",", "")]
+		[InlineData(",", "a", "a")]
+		[InlineData("*", "a*b", "a", "b")]
+		[InlineData(" ", "a b c", "a", "b", "c")]
+		[InlineData("", "abc", "a", "b", "c")]
+		public void Join(string separator, string expected, params string[] values)
+		{
+			Assert.Equal(expected, values.Join(separator));
+		}
+
+		[Theory]
+		[InlineData("", "and", false)]
+		[InlineData("c", "and", false, "c")]
+		[InlineData("a or b", "or", false, "a", "b")]
+		[InlineData("d, e, and even f", "and even", false, "d", "e", "f")]
+		[InlineData("d; e; f; g; and even h", "and even", true, "d", "e", "f", "g", "h")]
+		public void TextualJoin(string expected, string conjunction, bool complex, params string[] values)
+		{
+			Assert.Equal(expected, values.TextualJoin(conjunction, complex));
+		}
+	}
+}
